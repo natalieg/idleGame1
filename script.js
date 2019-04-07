32fa7c59da378f2e651618c0c4761b8f3c423fe5
Natalie <natalie.gornicki@gmail.com>
2019-04-07 20:57:31 +0200

    added a hunger bar, changed hunger behaviour, debug start and stop button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $ = jQuery;
 
+var time = 1000;
+var isGameActive = true;
+var myTimerVar;
+var counter = 1;
+
 var mood = 1;
 
 var maxHunger = 100,
@@ -10,23 +15,88 @@ var hungerTime = 1000;
 var maxEnergy = 100,
     currentEnergy = maxEnergy;
 
-var storageFood = 0,
-    maxStorageFood = 20;
+var storageFood = 2, // Testvalue
+    maxStorageFood = 20
+foodValue = 5;
+eatAmount = 1;
 var storageSticks = 0,
     maxStorageSticks = 20;
 
-
-function lowerNeed(need, needValue, time, className){   
-    setInterval(function(){
-        need = need - needValue;
-        $(className).html(need);
-    }, time);
+function changeGameStatus(newStatus) {
+    if (newStatus) {
+        myTimerVar = setInterval(globalTimer, time);
+    } else {
+        clearInterval(myTimerVar);
+        console.log("Stop Playing");
+    }
 }
 
+/**
+ * Checks if there is food in the storage
+ * if this is the case, the player is fed, 
+ * when no food is available, the hunger value
+ * is rising (in this case going down)
+ */
+function checkHungerAndFood() {
+    if (storageFood > 0) {
+        if ((currentHunger == maxHunger) && (counter % 4 == 0)) {
+            storageFood = storageFood - eatAmount;
+            console.log("Hunger is fulfilled");
+        } else if (currentHunger < maxHunger) {
+            currentHunger = currentHunger + foodValue;
+            if (currentHunger > maxHunger) {
+                currentHunger = maxHunger;
+            }
+            storageFood = storageFood - eatAmount;
+        }
+    } else {
+        if (currentHunger > 0) {
+            currentHunger = currentHunger - 1; //#MAYBE might decrease slower later? if it makes sense?
+        }
+    }
+
+    switch (true) {
+        case (currentHunger >= 51):
+            mood = 1;
+            $('.hungerBar').css("background-color", "green");
+            break;
+        case ((currentHunger < 51) && (currentHunger > 10)):
+            mood = 0;
+            $('.hungerBar').css("background-color", "yellow");
+            break;
+        case (currentHunger <= 10):
+            $('.hungerBar').css("background-color", "red");
+            break;
+        default: 
+        console.log("Something went wrong in the Switch");
+    }
+
+    $('.hungerBar').width(currentHunger);
+    $('.hunger').html(currentHunger);
+    $('.food').html(storageFood);
+
+    if (mood == 0) {
+        $('#youImg').attr("src", "img/youSad.jpg");
+    } else {
+        $('#youImg').attr("src", "img/you.jpg");
+    }
+}
+
+// Automatic Intervalls
+
+function globalTimer() {
+    checkHungerAndFood();
+    counter = counter + 1;
+    console.log("Counter: " + counter);
+    if (counter > 1000) {
+        counter = 0;
+    }
+}
 
 $(document).ready(function () {
 
     // Initialization
+    myTimerVar = setInterval(globalTimer, time);
     console.log("ready!");
 
     $('.hunger').html(maxHunger);
@@ -35,34 +105,14 @@ $(document).ready(function () {
     $('.food').html(storageFood);
     // $('.sticks').html("Sticks " + storageSticks);
 
-    // Automatic Intervalls
-    setInterval(function(){
-        if((storageFood > 0) && (currentHunger < maxHunger)) {
-            storageFood = storageFood - 1;
-            currentHunger = currentHunger + 5;
-            $('.food').html(storageFood);
-        } else {
-            currentHunger = currentHunger - 1;
-        }
-
-        $('.hunger').html(currentHunger);
-        if (currentHunger < 50){
-            mood = 0;
-        }
-    }, hungerTime);
-
-    setInterval(function(){
-        if (mood == 0){
-            $('#youImg').attr("src","img/youSad.jpg");
-        } else {
-            $('#youImg').attr("src","img/you.jpg");
-        }
-    }, 1000);
-
     // Actions
-    $(".collectFood").click(function(){
+    $(".collectFood").click(function () {
         storageFood = storageFood + 1;
         console.log("FOOD!!!!");
         $('.food').html(storageFood);
+    })
+    $(".stopStart").click(function () {
+        isGameActive = !isGameActive;
+        changeGameStatus(isGameActive);
     })
 });
